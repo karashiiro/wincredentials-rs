a5c0514bcc7d69b69b364d2e4f4ac64a62e37c93
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2025-12-02 11:33:30 +0100

    directly use windows API bindings in main crate

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,14 @@ mod tests;
 
 use std::ffi::c_void;
 use widestring::{U16CString, U16String};
-//use windows::*;
-
-use wincredentials_bindings::Windows::Win32::{
-    Foundation::*, Security::Credentials::*, System::SystemInformation::*,
+use windows::Win32::Security::Credentials::{
+    CRED_FLAGS, CRED_PERSIST, CRED_TYPE, CREDENTIALW, CredDeleteW, CredFree, CredReadW, CredWriteW,
 };
+use windows::Win32::System::SystemInformation::GetSystemTimeAsFileTime;
+use windows::core::PWSTR;
 
-const NO_FLAGS: u32 = 0;
-const GENERIC_CREDENTIAL: u32 = 1;
+const NO_FLAGS: CRED_FLAGS = CRED_FLAGS(0);
+const GENERIC_CREDENTIAL: CRED_TYPE = CRED_TYPE(1);
 
 /// Reads the credential with the specified target name. If the operation
 /// fails for any reason, including no credential existing, the result
@@ -27,10 +27,9 @@ pub fn read_credential(target: &str) -> Result<credential::Credential, Box<dyn s
         CredReadW(
             PWSTR(target_ptr as *mut u16),
             GENERIC_CREDENTIAL,
-            NO_FLAGS,
+            None,
             cred_ptr,
-        )
-        .ok()?
+        )?
     };
 
     // Read from the credential and convert it into something rustier
@@ -56,48 +55,47 @@ pub fn read_credential(target: &str) -> Result<credential::Credential, Box<dyn s
 
 /// Writes out the credential with the specified target name. If the operation
 /// fails for any reason, the result will resolve to an error.
-pub fn write_credential(target: &str, val: credential::Credential) -> Result<(), Box<dyn std::error::Error>> {
-    // Get the current time as a Windows file time
-    let filetime = Box::new(FILETIME {
-        dwLowDateTime: 0,
-        dwHighDateTime: 0,
-    });
-    let filetime: *mut FILETIME = Box::into_raw(filetime);
-    unsafe { GetSystemTimeAsFileTime(filetime) };
+pub fn write_credential(
+    target: &str,
+    val: credential::Credential,
+) -> Result<(), Box<dyn std::error::Error>> {
+    unsafe {
+        // Get the current time as a Windows file time
+        let filetime = GetSystemTimeAsFileTime();
 
-    // Convert all the things into UTF16
-    let secret_len = val.secret.len();
+        // Convert all the things into UTF16
+        let secret_len = val.secret.len();
 
-    let target_cstr = U16CString::from_str(target).unwrap();
-    let secret_cstr = U16CString::from_str(val.secret).unwrap();
-    let user_cstr = U16CString::from_str(val.username).unwrap();
+        let target_cstr = U16CString::from_str(target).unwrap();
+        let secret_cstr = U16CString::from_str(val.secret).unwrap();
+        let user_cstr = U16CString::from_str(val.username).unwrap();
 
-    let target_ptr = target_cstr.as_ptr();
-    let secret_ptr = secret_cstr.as_ptr();
-    let user_ptr = user_cstr.as_ptr();
+        let target_ptr = target_cstr.as_ptr();
+        let secret_ptr = secret_cstr.as_ptr();
+        let user_ptr = user_cstr.as_ptr();
 
-    // Build our credential object
-    let cred = CREDENTIALW {
-        Flags: CRED_FLAGS(NO_FLAGS),
-        Type: CRED_TYPE(GENERIC_CREDENTIAL),
-        TargetName: PWSTR(target_ptr as *mut u16),
-        Comment: PWSTR(std::ptr::null_mut() as *mut u16),
-        LastWritten: unsafe { *filetime },
-        CredentialBlobSize: secret_len as u32 * 2,
-        CredentialBlob: secret_ptr as *mut u8,
-        Persist: CRED_PERSIST(1),
-        AttributeCount: 0,
-        Attributes: std::ptr::null_mut(),
-        TargetAlias: PWSTR(std::ptr::null_mut() as *mut u16),
-        UserName: PWSTR(user_ptr as *mut u16),
+        // Build our credential object
+        let cred = CREDENTIALW {
+            Flags: NO_FLAGS,
+            Type: GENERIC_CREDENTIAL,
+            TargetName: PWSTR(target_ptr as *mut u16),
+            Comment: PWSTR(std::ptr::null_mut() as *mut u16),
+            LastWritten: filetime,
+            CredentialBlobSize: secret_len as u32 * 2,
+            CredentialBlob: secret_ptr as *mut u8,
+            Persist: CRED_PERSIST(1),
+            AttributeCount: 0,
+            Attributes: std::ptr::null_mut(),
+            TargetAlias: PWSTR(std::ptr::null_mut() as *mut u16),
+            UserName: PWSTR(user_ptr as *mut u16),
+        };
+
+        // Write the credential out
+        CredWriteW(&cred, NO_FLAGS.0)?
     };
 
-    // Write the credential out
-    unsafe { CredWriteW(&cred, NO_FLAGS).ok()? };
-
     // Free the file time object we got
-    unsafe { drop(Box::from_raw(filetime)) }
-
+    //unsafe { drop(Box::from_raw(filetime)) }
     Ok(())
 }
 
@@ -109,7 +107,7 @@ pub fn delete_credential(target: &str) -> Result<(), Box<dyn std::error::Error>>
     let target_ptr = target_cstr.as_ptr();
 
     // Delete the credential
-    unsafe { CredDeleteW(PWSTR(target_ptr as *mut u16), GENERIC_CREDENTIAL, NO_FLAGS).ok()? };
+    unsafe { CredDeleteW(PWSTR(target_ptr as *mut u16), GENERIC_CREDENTIAL, None)? };
 
     Ok(())
 }
