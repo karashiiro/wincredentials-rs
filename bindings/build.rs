4c91404e2cc082ae8ffa3f29705f19144b36140d
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2025-12-02 11:34:50 +0100

    remove obsolete crate project

--- bindings/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    windows::build! {
-        Windows::Win32::Foundation::{BOOL, PWSTR, FILETIME},
-        Windows::Win32::Security::Credentials::{CredReadW, CredWriteW, CredDeleteW, CredFree, CREDENTIALW, CRED_FLAGS, CRED_TYPE, CRED_PERSIST},
-        Windows::Win32::System::SystemInformation::GetSystemTimeAsFileTime,
-    };
-}
